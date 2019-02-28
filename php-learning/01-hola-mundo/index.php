564c6df51b22ff3c56292d96b30e75428491943e
followersito <45855921+followersito@users.noreply.github.com>
2019-02-28 10:08:25 -0600

    Archivos de PHP

---
+++ php-learning/01-hola-mundo/index.php
@@ -0,0 +1,4 @@
+<h1>Hola mundo</h1>
+<?php
+echo '<h2>Hola mundo, con PHP 7<h2>';
+?>
---
+++ php-learning/02-print/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Imprimir por pantalla - Master en PHP</title>
+    </head>
+    <body>
+        <h1>Master en PHP - Carlos Iván</h1>
+        <?php
+            // Titulo de la sección
+            echo "<h3>Listado de videojuegos:</h3>";
+            echo "<ul>"
+                    .   "<li>GTA</li>"
+                    .   "<li>Fifa</li>"
+                    .   "<li>CS:GO</li>"
+                    . "</ul>";
+            /*
+            echo '<br>HOLAAAAAAAAAAAAA<br>';
+            */
+             //Ejemplo de párrafo 
+            echo '<p>Esta es toda'.' - '.'lista de juegos </p>';
+        ?>
+    </body>
+</html>
