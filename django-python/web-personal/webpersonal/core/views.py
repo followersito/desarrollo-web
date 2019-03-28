652e0b2ab85b061903947d0aebcf510a6ebd9435
followersito <45855921+followersito@users.noreply.github.com>
2019-03-28 10:58:23 -0600

    Archivos html con herencia

--- django-python/web-personal/webpersonal/core/views.py
+++ django-python/web-personal/webpersonal/core/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
+
 # Create your views here.
 def home(request):
-   # html_response = "<h1> Mi web personal</h1>"
-   # for i in range(10):
-      #  html_response += "<h2>Portada</h2>"
-    return HttpResponse(html_base+"""
-    <h2>Esto es la portada</h2>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base+"<h1>Acerca de</h1><p>Me llamo Ivan<p>")
+    return render(request, 'core/about-me.html')
 
 def portfolio(request):
-    return HttpResponse(html_base + "<h1>Portafolio</h1><p>Esta es la pagina de portafolio<p>")
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(html_base+"<h1>Contacto</h1><p>Este es el apartado de contacto<p>")
+    return render(request, 'core/contact.html')
