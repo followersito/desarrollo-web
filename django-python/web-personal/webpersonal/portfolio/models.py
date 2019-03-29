d44d7b386676fd91f39c6811ce21125cb8c7cd8d
followersito <45855921+followersito@users.noreply.github.com>
2019-03-29 09:52:14 -0600

    Se añadió la app Portfolio

--- django-python/web-personal/webpersonal/portfolio/models.py
+++ django-python/web-personal/webpersonal/portfolio/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)          # Campo de cadena de caracteres
-    description = models.TextField()            # Campo de texto
-    image = models.ImageField()         # Campo de imagen
-    created = models.DateTimeField(auto_now_add = True)         # Campo de fecha y hora (Se añade automaticamente al crear instancias)
-    updated = models.DateTimeField(auto_now = True)             # Se actualiza cuando se modifica
+    title = models.CharField(max_length=200, verbose_name="Titulo")          # Campo de cadena de caracteres
+    description = models.TextField(verbose_name="Descripción")            # Campo de texto
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")         # Campo de imagen
+    url_field = models.URLField(max_length=200, verbose_name="URL", blank=True, null=True)         # blank y null permiten el campo como Opcional
+    created = models.DateTimeField(auto_now_add = True,verbose_name="Fecha de creación")         # Campo de fecha y hora (Se añade automaticamente al crear instancias)
+    updated = models.DateTimeField(auto_now = True,verbose_name="Fecha de edición")             # Se actualiza cuando se modifica
+   
 
+    # Las clases y métodos declarados acá son para configuración extra a la clase en front end
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]         # Se pone el guión para que se haga el orden a la inversa
+
+    def __str__(self):
+        return self.title
--- django-python/web-personal/webpersonal/portfolio/views.py
+++ django-python/web-personal/webpersonal/portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def portfolio(request):
+    project_list = Project.objects.all()
+    return render(request, 'portfolio/portfolio.html', {'projects':project_list})
+
