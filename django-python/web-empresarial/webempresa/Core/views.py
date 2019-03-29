799d4f49fcad548463283a788d97e9619e621a05
followersito <45855921+followersito@users.noreply.github.com>
2019-03-29 11:10:19 -0600

    Plantillas de front end acopladas

--- django-python/web-empresarial/webempresa/Core/views.py
+++ django-python/web-empresarial/webempresa/Core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "Core/index.html")
+
+def about(request):
+    return render(request, "Core/about.html")
+
+def services(request):
+    return render(request, "Core/services.html")
+
+def store(request):
+    return render(request, "Core/store.html")
+
+def contact(request):
+    return render(request, "Core/contact.html")
+
+def blog(request):
+    return render(request, "Core/blog.html")
+
+def sample(request):
+    return render(request, "Core/sample.html")
