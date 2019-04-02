d092a13c8ebe9de4ef7ceb90c344fd93cb98a043
followersito <45855921+followersito@users.noreply.github.com>
2019-04-02 14:51:19 -0600

    Logins / Permisos

--- django-python/webplayground/webplayground/urls.py
+++ django-python/webplayground/webplayground/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    # Paths de autenticación
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
